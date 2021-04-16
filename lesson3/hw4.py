fb3bfe1417d7900be1f5982a3bbd44cfc8b4577f
MaJluc <79608738+MaJluc@users.noreply.github.com>
2021-04-16 17:04:19 +0300

    Update hw4.py

--- lesson3/hw4.py
+++ lesson3/hw4.py
@@ -1,47 +1,50 @@
 import pathlib
 import sys
-def recursive_print(path):
-    pic=[]
-    vid=[]
-    doc=[]
-    musc=[]
-    arch=[]
-    unknown=[]
-    file_type=set()
-    unknown_type=set()
+pic=[]
+vid=[]
+doc=[]
+musc=[]
+arch=[]
+unknown=[]
+file_type=set()
+unknown_type=set()
+def sort_files(path):
+    result = ''
+    global pic
+    global vid
+    global doc
+    global musc
+    global arch
+    global unknown
+    global file_type
+    global unknown_type
     if path.is_dir():
         for element in path.iterdir():
-            recursive_print(element)
+            sort_files(element)
     else:
-        if path.suffix(path) in ('JPEG', 'PNG', 'JPG', 'SVG'):
+        if path.suffix in ('.jpg', '.png', '.jpeg', '.svg'):
             pic.append(path.name)
             file_type.add(path.suffix)
-        elif path.suffix(path) in ('AVI', 'MP4', 'MOV', 'MKV'):
+        elif path.suffix in ('.avi', '.mp4', '.mov', '.mkv'):
             vid.append(path.name)
             file_type.add(path.suffix)
-        elif path.suffix(path) in ('DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX'):
+        elif path.suffix in ('.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx'):
             doc.append(path.name)
             file_type.add(path.suffix)
-        elif path.suffix(path) in ('MP3', 'OGG', 'WAV', 'AMR'):
+        elif path.suffix in ('.mp3', '.ogg', '.wav', '.amr'):
             musc.append(path.name)
             file_type.add(path.suffix)
-        elif path.suffix(path) in ('ZIP', 'GZ', 'TAR'):
+        elif path.suffix in ('.zip', '.gz', '.tar'):
             arch.append(path.name)
             file_type.add(path.suffix)
         else:
             unknown.append(path.name)
             unknown_type.add(path.suffix)
-    return pic
-    return vid
-    return doc
-    return musc
-    return arch
-    return unknown
-    return file_type
-    return unknown_type
+    result = f"В папке лежат файлы с известными расширениями: {file_type} \nи неизвестными расширениями: {unknown_type} \nИз них картинки:\n{pic}\nвидео:\n{vid}\nдокументы:\n{doc}\nархивы:\n{arch}\nмузыка:\n{musc}\nнепонятные файлы:\n{unknown}"
+    return result
 def main():
     path = sys.argv[1]
     path = pathlib.Path(path)
-    recursive_print(path)
-if __name__ == '__main':
+    print(sort_files(path))
+if __name__ == '__main__':
     main()
